faacbe6b0698bfca1f25bb415924e03d9154d5ba
김성민 <98kimsungmin@naver.com>
2021-10-07 14:05:24 +0000

    211007

---
+++ frontend/register.js
@@ -0,0 +1,16 @@
+function div_army() {
+    divArmy = document.getElementById("army");
+    divArmy.style.display = "block";
+
+    divAirforce = document.getElementById("airforce");
+    divAirforce.style.display = "none";
+}
+
+function div_airforce() {
+    divArmy = document.getElementById("army");
+    divArmy.style.display = "none";
+
+    divAirforce = document.getElementById("airforce");
+    divAirforce.style.display = "block";
+
+}

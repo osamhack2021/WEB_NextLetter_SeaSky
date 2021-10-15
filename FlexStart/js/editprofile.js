c4cc061b7f9d8eae9e34e3fce8c756f79ade1a8c
CKIRUser <CKIRUser@W43518>
2021-10-15 15:14:50 +0900

    21.10.15 15:14 login, register, index 수정

--- FlexStart/js/editprofile.js
+++ FlexStart/js/editprofile.js
@@ -2,13 +2,31 @@ function saveInfo(nameList){
 	var list = nameList.split(',');
 	for(var i in list){
 		var name = list[i];
-		var value = document.getElementsByName(name)[0].value;
+		var value = '';
+		if(name == 'grpCdNm'){
+			value = document.querySelector('input[name="' + name + '"]:checked').value;
+			
+		}else if(name == 'trainUnitCdNm'){
+			value = $('select[name='+ name + ']').val();
+		}else{
+			var item = document.getElementsByName(name)[0];
+			if(typeof item != 'undefined' && item != null && item != ''){
+				value = item.value;
+			}else{
+				alert(name + ' 항목을 채워주세요!');
+				return false;
+			}
+			
+		}
+		 
 		
-		if(typeof value != "undefined" && value != null && value != ""){
+		if(typeof value != 'undefined' && value != null && value != ''){
 			setSession(name, value);
 		}
 		
 	}
+
+	alert('저장되었습니다.');
 }
 
 
@@ -19,9 +37,19 @@ function setInfo(keyList){
 	for(var i in list){
 		var key = list[i];
 		var value = getSession(key);
-		if(typeof value != "undefined" && value != null && value != ""){
+		if(typeof value != 'undefined' && value != null && value != ''){
+			if(key == 'grpCdNm'){
+				if(value == 'army'){
+					$("#army_radio").trigger("click");
+				}else if(value == 'airforce'){
+					$("#airforce_radio").trigger("click");
+				}
+			}else if(name == 'trainUnitCdNm'){
+				$("#trainUnitCdNm").text(value).attr("selected", "selected");
+			}else{
+				document.getElementsByName(key)[0].value = value;
+			}
 			
-			document.getElementsByName(key)[0].value = value;
 		}
 	}
 	

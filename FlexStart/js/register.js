e34e26edcefd020ccb1c97b594e419db044f1136
CKIRUser <CKIRUser@W43518>
2021-10-15 23:57:22 +0900

    21.10.15 23:56 hyungi modify edit-profile/index/register

--- FlexStart/js/register.js
+++ FlexStart/js/register.js
@@ -1,20 +1,16 @@
 function div_army() {
-    document.getElementById("army").style.display = "block";
+    document.getElementById('army').style.display = 'block';
 
-    document.getElementById("airforce").style.display = "none";
     
-
-    document.getElementById("Submit").style.display = "none";
-    document.getElementById("armySubmit").style.display = "block";
-    document.getElementById("airforceSubmit").style.display = "none";
+    document.getElementById('Submit').style.display = 'none';
+    document.getElementById('armySubmit').style.display = 'unset';
+    document.getElementById('airforceSubmit').style.display = 'none';
 }
 
 function div_airforce() {
-    document.getElementById("army").style.display = "none";
-
-    document.getElementById("airforce").style.display = "block";
+    document.getElementById('army').style.display = 'none';
     
-    document.getElementById("Submit").style.display = "none";
-    document.getElementById("armySubmit").style.display = "none";
-    document.getElementById("airforceSubmit").style.display = "block";
+    document.getElementById('Submit').style.display = 'none';
+    document.getElementById('armySubmit').style.display = 'none';
+    document.getElementById('airforceSubmit').style.display = 'unset';
 }
